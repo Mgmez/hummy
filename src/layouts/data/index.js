227ebb24111df3d63adf7f0cc0ecbc694ce3c12c
Jose Aguayo <jmiguel.aguayo@alumnos.udg.mx>
2023-02-04 19:29:46 -0600

    Create hummy app

---
+++ src/layouts/data/index.js
@@ -0,0 +1,15 @@
+// Material Dashboard 2 React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+
+function Data() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Data;
